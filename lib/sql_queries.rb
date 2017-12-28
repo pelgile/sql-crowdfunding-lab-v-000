f151eb68380371f74d8083945ac24a05c1424ca4
Michelle Wiraydi <miwiryadi@gmail.com>
2017-12-28 17:07:34 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -21,14 +21,3 @@ end
 def selects_the_category_name_and_the_sum_total_of_the_all_its_pledges_for_the_book_category
   "SELECT projects.category, SUM(pledges.amount) FROM projects JOIN pledges ON projects.id = pledges.project_id WHERE projects.category = 'books' GROUP BY projects.category;"
 end
-© 2017 GitHub, Inc.
-Terms
-Privacy
-Security
-Status
-Help
-Contact GitHub
-API
-Training
-Shop
-Blog
